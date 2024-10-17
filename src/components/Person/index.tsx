4f73b62c760e653576af3166a3bce189d97d7d20
Rael CALITRO <rael06@hotmail.fr>
2024-10-17 15:52:15 +0200

    v0.1

---
+++ src/components/Person/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+type PersonType = {
+  name: string;
+  age: number;
+};
+
+export default function Person() {
+  const alice: PersonType = { name: "Alice", age: 18 };
+  const [person, setPerson] = useState(alice);
+  const type = person.age < 18 ? "Child" : "Adult";
+
+  const incrementAge = () => {
+    const updatedPerson = {
+      ...person,
+      age: person.age + 1,
+    };
+    setPerson(updatedPerson);
+  };
+
+  const decrementAge = () => {
+    const updatedPerson = {
+      ...person,
+      age: person.age - 1,
+    };
+    setPerson(updatedPerson);
+  };
+
+  return (
+    <div>
+      <button onClick={decrementAge}>Decrement</button>
+      <button onClick={incrementAge}>Increment</button>
+      <div>
+        <p>Name: {person.name}</p>
+        <p>Age: {person.age}</p>
+        <p>Type: {type}</p>
+      </div>
+    </div>
+  );
+}
